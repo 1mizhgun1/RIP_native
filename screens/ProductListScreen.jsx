33bc28a02ee6f51ce5e70579938af3b458c85e2f
mizhgun <m.a.ugryumov@outlook.com>
2023-12-18 00:28:21 +0300

    product screen

--- screens/ProductListScreen.jsx
+++ screens/ProductListScreen.jsx
@@ -5,8 +5,11 @@ import { axiosInstance } from '../API';
 import { setProducts } from '../store/productSlice';
 import ProductCard from '../components/ProductCard';
 import Filter from '../components/Filter';
+import Loading from '../components/Loading';
 
 export default function ProductListScreen({ navigation }) {
+    const [ loading, setLoading ] = useState(true);
+
     const dispatch = useDispatch();
     const { products } = useSelector((store) => store.product);
 
@@ -21,12 +24,20 @@ export default function ProductListScreen({ navigation }) {
     useEffect(() => {
         async function getProducts() {
             await axiosInstance.get(`/products/?status=A&title=${searchValue}&price_min=${price_min()}&price_max=${price_max()}`)
-                .then((response) => dispatch(setProducts(response?.data)));
+                .then((response) => dispatch(setProducts(response?.data.products)));
         }
-        getProducts(); 
+
+        getProducts().then(() => {
+            setLoading(false)
+        }).catch((error) => {
+            console.log(error)
+            setLoading(false)
+        });
+
     }, [dispatch, filterSendCount]);
 
     return (
+        !loading ?
         <ScrollView>
             <Filter
                 search={searchValue}
@@ -38,10 +49,11 @@ export default function ProductListScreen({ navigation }) {
                 send={setFilterSendCount}
             />
             <View style={styles.page}>
-                {!!products && !!products.products &&
-                    products.products.map((product, index) => <ProductCard key={index} {...product} navigation={navigation} />)}
+                {!!products &&
+                    products.map((product, index) => <ProductCard key={index} {...product} navigation={navigation} />)}
             </View>
         </ScrollView>
+        : <Loading />
     );
 }
 
--- screens/ProductScreen.jsx
+++ screens/ProductScreen.jsx
@@ -3,8 +3,11 @@ import { useDispatch, useSelector } from 'react-redux';
 import React, { useState, useEffect } from 'react';
 import { resetProduct, setProduct } from '../store/productSlice';
 import { axiosInstance } from '../API';
+import Loading from '../components/Loading';
 
 export default function ProductScreen({ route }) {
+    const [ loading, setLoading ] = useState(true);
+
     const { id } = route.params;
     const dispatch = useDispatch();
     const { product } = useSelector((store) => store.product);
@@ -25,12 +28,17 @@ export default function ProductScreen({ route }) {
     useEffect(() => {
         async function getProduct() {
             await axiosInstance.get(`/products/${id}`).then((response) => {
-                dispatch(setProduct(response?.data))
+                dispatch(setProduct(response?.data));
                 setParameters(getParams(response?.data));
             });
         }
 
-        getProduct()
+        getProduct().then(() => {
+            setLoading(false)
+        }).catch((error) => {
+            console.log(error)
+            setLoading(false)
+        });
 
         return () => {
             dispatch(resetProduct());
@@ -38,38 +46,117 @@ export default function ProductScreen({ route }) {
     }, [dispatch]);
 
     return (
+        !loading ?
         <View style={styles.page}>
-            <Text>{product.title}</Text>
-            <Image source={{ uri: product.image }} resizeMode='contain'/>
-            <Text>{product.price} ₽</Text>
-            <Text>Характеристики</Text>
+            <Text style={styles.title}>{product.title}</Text>
+            {product.cnt > 0 ?<Text style={styles.status_green}>в наличии</Text> : <Text style={styles.status_red}>раскупили</Text>}
+            <Image style={styles.image} source={{ uri: product.image }} resizeMode='contain'/>
+            <Text style={styles.price}>{product.price} ₽</Text>
+            <Text style={styles.param_title}>Характеристики</Text>
             <View style={styles.container}>
                 {parameters.map((parameter, index) => (
                     <View key={index} style={styles.row}>
                         <View style={styles.column}>
-                            <Text>{parameter.key}</Text>
+                            <Text style={styles.param_text}>{parameter.key}</Text>
+                            <View style={styles.param_dots}></View>
                         </View>
                         <View style={styles.column}>
-                            <Text>{parameter.value}</Text>
+                            <Text style={styles.param_text}>{parameter.value}</Text>
                         </View>
                     </View>
                 ))}
             </View>
         </View>
+        : <Loading />
     );
 }
 
 const styles = StyleSheet.create({
+    page: {
+        backgroundColor: '#ffffff',
+        display: 'flex',
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+    },
+    title: {
+        color: '#006bd5',
+        marginLeft: 20,
+        marginRight: 20,
+        marginTop: 10,
+        fontSize: 20,
+        textAlign: 'center',
+    },
+    price: {
+        fontSize: 28,
+        fontWeight: '300',
+        margin: 5,
+        textAlign: 'center',
+    },
+    image: {
+        height: 250,
+        alignSelf: 'stretch',
+        marginLeft: 20,
+        marginRight: 20,
+    },
+    param_title: {
+        marginTop: 25,
+        padding: 4,
+        backgroundColor: '#00b90e',
+        fontWeight: '500',
+        fontSize: 22,
+        color: '#ffffff',
+        marginLeft: 20,
+        marginRight: 20,
+        textAlign: 'center',
+    },
+    param_text: {
+        fontSize: 16,
+    },
+    param_dots: {
+        flex: 1,
+        position: 'relative',
+        marginHorizontal: 3,
+        top: -3,
+        height: '100%',
+        borderStyle: 'dotted',
+        borderBottomWidth: 2,
+        borderBottomColor: '#777777',
+    },
+    status_green: {
+        textAlign: 'center',
+        marginVertical: 15,
+        padding: 6,
+        textTransform: 'uppercase',
+        backgroundColor: '#00b90e',
+        fontWeight: 'bold',
+        color: '#ffffff',
+        marginLeft: 145,
+        marginRight: 145,
+    },
+    status_red: {
+        textAlign: 'center',
+        marginVertical: 15,
+        padding: 6,
+        textTransform: 'uppercase',
+        backgroundColor: '#ff7200',
+        fontWeight: 'bold',
+        marginLeft: 145,
+        marginRight: 145,
+    },
     container: {
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'center',
+        padding: 20,
     },
     row: {
         flexDirection: 'row',
     },
     column: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
         flex: 1,
-        padding: 10,
+        paddingBottom: 20,
     },
   });
